8ad2a5a8052e769bfdb2dde478a92f380895c975
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2023-03-25 07:06:46 -0600

    Adjusted integrator to use a single rate and many seasons, instead of each rate having its own season. This fixes the bug of pricelabs not allowing bookings longer than a single night.

--- integrations/integrator.py
+++ integrations/integrator.py
@@ -4,8 +4,9 @@ from requests.auth import HTTPBasicAuth
 import json
 
 
-def integrate(testing, moto_key, moto_secret, season_request, rates_request, pricelabs_api_key, pricelabs_id, accomodation_id):
-    # Info for http request
+def integrate(testing, moto_key, moto_secret, season_request, rates_request, pricelabs_api_key, pricelabs_id,
+         accomodation_id):
+
     basic = HTTPBasicAuth(moto_key, moto_secret)
 
     # List of all dates we need to keep track of.
@@ -16,6 +17,7 @@ def integrate(testing, moto_key, moto_secret, season_request, rates_request, pri
         existing_seasons = get_data(season_request,
                                     basic)  # One thing to note, is this will come back as empty if the ones in there are only drafts and not published.
         existing_rates = get_data(rates_request, basic)
+        rates_id = existing_rates[0]['id']
 
         current_dates = [i['end_date'] for i in existing_seasons]
         to_add = [date for date in list_dates if date not in current_dates]
@@ -25,38 +27,43 @@ def integrate(testing, moto_key, moto_secret, season_request, rates_request, pri
 
         for i in to_add:
             pass
-            season_response = create_season(i, season_request, basic)
-            # print('season response: ', season_response.json())
-            create_rate(season_response.json(), rates_request, basic, accomodation_id)
+            create_season(i, season_request, basic)
 
         for date in to_delete:
             season_id = delete_season(season_request, date, existing_seasons,
                                       basic)  # I should make this return the season id so we can pass it through to delete the rate.
-            if not season_id is None:  # if the season_id is not null, then it returned the season and we can delete the rate associated with it
-                pass
-                delete_rate(rates_request, season_id, existing_rates, basic)
 
         # After any seasons are added or deleted, we need to refresh the existing lists with updated data.
         existing_seasons = get_data(season_request,
                                     basic)
         existing_rates = get_data(rates_request, basic)
-
+        rates_id = existing_rates[0]['id']
         motopress_data = motopress_rates_data(existing_seasons,
-                                              existing_rates)  # Returns a dictionary with end date, rates_id, price, season_id.
-        pricelabls_data = get_pricelabs_data(pricelabs_api_key, pricelabs_id)  # will return a dict with date as key and price as value
-        compare_prices(motopress_data, pricelabls_data, rates_request, basic)
+                                              existing_rates)  # Returns a dictionary with end date as key, season_id as value.
+        pricelabls_data = get_pricelabs_data(pricelabs_api_key,
+                                             pricelabs_id)  # will return a dict with date as key and price as value
+        moto_data_to_add = compare_prices(motopress_data, pricelabls_data, rates_request, basic)
+        create_rate(moto_data_to_add, rates_request, basic, accomodation_id, rates_id)
 
     else:
         pass
         # *** For Testing ***
-        # existing_seasons = get_data(season_request, basic) # I made a copy of this one and renamed it to have find_duplicates at the end.
-        #
-        # current_dates = [i['end_date'] for i in existing_seasons]
-        # to_add = [date for date in list_dates if date not in current_dates]
-        # to_delete = [date for date in current_dates if date not in list_dates]
-        # print('to_add: ', to_add)
-        # print('to_delete: ', to_delete)
-        print("This is the testing section of function.")
+        existing_seasons = get_data(season_request,
+                                    basic)  # I made a copy of this one and renamed it to have find_duplicates at the end.
+
+        existing_rates = get_data(rates_request, basic)
+        rates_id = existing_rates[0]['id']
+
+
+        motopress_data = motopress_rates_data(existing_seasons,
+                                              existing_rates)  # Returns a dictionary with end date as key, season_id as value.
+
+        pricelabls_data = get_pricelabs_data(pricelabs_api_key,
+                                             pricelabs_id)  # will return a dict with date as key and price as value
+
+        moto_data_to_add = compare_prices(motopress_data, pricelabls_data, rates_request, basic)
+        create_rate(moto_data_to_add, rates_request, basic, accomodation_id, rates_id)
+
 
         # *** End testing ***
 
@@ -96,70 +103,52 @@ def compare_prices(motopress, pricelabs, rates_request,
     motopress and pricelabs rates. If they are different, it sends a post request to motopress to update the rate.'''
     # print('motopress', motopress)
 
-    for data in motopress:
+    rate_data_to_add = []
+    priority = 0
+    # print(motopress)
+    for data, season_id in motopress.items():
         date = data
-        price = motopress[date][0]
-        rate_id = motopress[date][1]
-        season_id = motopress[date][2]
-        # print(date, price, rates_id)
-        # if motopress[i][0]
-        # print(motopress[i][0])
-
-        # pricelabs[date] = output is the price per date
+        # print('date from compare_prices', data)
+        # print('id from compare_prices', season_id)
 
         if date in pricelabs:
             pricelabs_rate = pricelabs[date]  # output is the rate shown in pricelabs
 
-            if pricelabs_rate != price:
-                print(f'price is different for {date}')
-                update_price(pricelabs_rate, rate_id, season_id, rates_request, basic)
-
-
-def update_price(rate, rate_id, season_id, url, basic):
-    '''Sends a post request to motopress to update the nightly rate for that date.'''
-    text = {
-        "status": "active",
-        "season_prices": [
-            {
-                "priority": 0,
-                "base_price": rate,
+            entry = {
+                "priority": priority,
                 "season_id": season_id,
+                "base_price": pricelabs_rate,
                 "variations": [
                     {
                         "adults": 11,
                         "children": 0,
-                        "price": rate + 25
+                        "price": pricelabs_rate + 25
                     },
                     {
                         "adults": 12,
                         "children": 0,
-                        "price": rate + 50
+                        "price": pricelabs_rate + 50
                     },
                     {
                         "adults": 13,
                         "children": 0,
-                        "price": rate + 75
+                        "price": pricelabs_rate + 75
                     }
                 ]
             }
-        ]
-    }
+            rate_data_to_add.append(entry)
+            priority = priority + 1
+    return rate_data_to_add
 
-    response = requests.post(url + str(rate_id), auth=basic, json=text)
-    print('status code', response.status_code)
-    print('response', response.json())
 
 
 def motopress_rates_data(existing_seasons, existing_rates):
     '''Uses the existing seasons and existing rates data to compile a dictionary that is used later to compare
     the motopress existing rates to what pricelabs has. It returns a dictionary with the key as the date and the
-    values are a list of the current existing price, the rate id, and the season id.'''
+    value is the season id.'''
 
     # print('existing seasons: ', existing_seasons)
-    # print('existing rates', existing_rates)
 
-    # dict will have date as key, price, then rate_id
-    # end_date: [price, rate_id]
     moto_dict = {}
 
     for season in existing_seasons:
@@ -168,9 +157,7 @@ def motopress_rates_data(existing_seasons, existing_rates):
         end_date = season['end_date']
         # print(season_id, end_date)
 
-        for rate in existing_rates:
-            if rate['season_prices'][0]['season_id'] == season_id:
-                moto_dict[end_date] = [rate['season_prices'][0]['base_price'], rate['id'], season_id]
+        moto_dict[end_date] = season_id
 
     # print(moto_dict)
     return moto_dict
@@ -180,7 +167,7 @@ def create_list_of_dates():
     '''Creats a list of dates thats used as a base to know if any dates need to be added to motopress or removed.'''
     list_dates = []
     for i in range(-1, 366):
-        # for i in range(-2, 13):
+    # for i in range(-1, 13):
         date_i = datetime.datetime.today() + datetime.timedelta(days=i)
         date_string = date_i.strftime('%Y-%m-%d')
         list_dates.append(date_string)
@@ -267,34 +254,30 @@ def create_season(season_date, url, basic):
     return response
 
 
-def create_rate(season_info, url, basic, accomodation_id):
+def create_rate(season_info, url, basic, accomodation_id, rates_id):
+    # print(season_stuff_to_add)
+    # print('season_prices:', season_prices)
 
     text = {
+        "id": 4186,
         "status": "active",
-        "title": f"{season_info['end_date']} - main post request",
-        "description": f"{season_info['end_date']} - main post request",
+        "title": "Bearadise Booking Rate",
+        "description": "Single rate - main post request",
         "accommodation_type_id": accomodation_id,
-        "season_prices": [
-            {
-                "priority": 0,
-                "base_price": 350,
-                "season_id": season_info['id'],
-                "variations": [
-
-                ]
-            }
-        ]
+        "season_prices": season_info
     }
 
     # print(text)
-    print('season_info', season_info)
+    # print('season_info', season_info)
+    url = url + str(rates_id) + '/'
     response = requests.post(url, json=text, auth=basic)
     print(response.status_code)
-    print(response.text)
+    # print(response.text)
     return response
 
 
 def get_pricelabs_data(api_key, id):
+    # API key and id are hard coded
     url = "https://api.pricelabs.co/v1/listing_prices"
 
     payload = json.dumps({
